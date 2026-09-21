9fb440b0c04866e9c6e5d88277e8b4b7037f7653
Diery Rivera <drive161@wgu.edu>
2026-09-21 10:12:39 -0400

    Init commit

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// fetch('https://pokeapi.co/api/v2/pokemon/pikachu') .then((response) => response.json()) .then(json=>{
+//     console.log(json)
+//     document.getElementById ("pikachu_profile") .src = json.sprites.front_shiny
+// })
+var slideshow = []
+var i = 0;
+async function get() {
+    var data = null
+    var res = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+
+data = await res.json()
+
+for (let i = 2; i < Object.keys(data.sprites).length; i++) {
+    var list = Object.keys(data.sprites)
+    slideshow.push(data.sprites[list[i]])
+    
+}
+function next() {
+    if (i == 7) return
+    document.getElementById ("pikachu_profile") .src = slideshow[i]
+    i++
+}
+function previous() {
+    if (i == 0) return 
+    document.getElementById("pikachu_profile") .src = slideshow[i]
+    i--
+}
+    document.getElementById("Next").onclick = next
+    document.getElementById("Previous").onclick = previous
+}
+get()
+
